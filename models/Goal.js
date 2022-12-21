d2c26f537ffa7c29a56f98b069d658e89e47275b
Isaac Luton <isaacdluton@gmail.com>
2022-12-20 20:12:43 -0800

    setup connection. add seeds. create server.js

---
+++ models/Goal.js
@@ -0,0 +1,44 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connections');
+
+class Goal extends Model {}
+
+Goal.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    completed: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'goal',
+  }
+);
+
+module.exports = Goal;
